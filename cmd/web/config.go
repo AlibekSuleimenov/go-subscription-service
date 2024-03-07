127d72f9a97482e34bc0be05087decc086dde00d
Alibek Suleimenov <alibek443@gmail.com>
2024-03-07 19:41:18 +0500

    initial commit: subscription service project

---
+++ cmd/web/config.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/alexedwards/scs/v2"
+	"github.com/alibeksuleimenov/subscription-service/data"
+	"log"
+	"sync"
+)
+
+type Config struct {
+	Session       *scs.SessionManager
+	DB            *sql.DB
+	InfoLog       *log.Logger
+	ErrorLog      *log.Logger
+	Wait          *sync.WaitGroup
+	Models        data.Models
+	Mailer        Mail
+	ErrorChan     chan error
+	ErrorChanDone chan bool
+}
